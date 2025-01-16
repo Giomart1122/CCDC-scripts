ae5faabcd5265c7076f0e74736a24941f4dd40c4
Gio M <151098531+Giomart1122@users.noreply.github.com>
2025-01-16 12:39:08 -0600

    Update install-ldap-alma.sh

--- install-ldap-alma.sh
+++ install-ldap-alma.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-# Update system and install LDAP packages on AlmaLinux
-sudo dnf update -y
+#remember to add alma or default user to sudoers file first before preceding
+# Install necessary package with ldap-server
+sudo dnf install epel-release
+# install LDAP packages on AlmaLinux
+# sudo dnf update -y (make sure u did this upon boot)
 sudo dnf install -y openldap openldap-servers openldap-clients
 sudo systemctl enable slapd
 sudo systemctl start slapd
@@ -11,4 +13,4 @@ sudo firewall-cmd --permanent --add-service=ldap
 sudo firewall-cmd --reload
 
 #show status
-sudo systemctl show slapd
+sudo systemctl status slapd
