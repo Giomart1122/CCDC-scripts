d96987774337308129bbd55375350acb92496592
Giovanni Martinez <151098531+Giomart1122@users.noreply.github.com>
2025-03-01 23:59:24 -0600

    Update change_user_pass.sh

--- change_user_pass.sh
+++ change_user_pass.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ldappasswd -x -D "cn=Manager,dc=ccdcldap,dc=local" -W -s <new password unhashed> "uid=lharris,ou=People,dc=ccdcldap,dc=local"
+
+#verify the change
+#ldapwhoami -x -D "uid=lharris,ou=People,dc=ccdcldap,dc=local" -W
