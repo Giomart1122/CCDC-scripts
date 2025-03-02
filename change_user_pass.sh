4d854c0e1f40632312794df5387111ebe4a25377
Giovanni Martinez <151098531+Giomart1122@users.noreply.github.com>
2025-03-01 23:57:39 -0600

    Update change_user_pass.sh

--- change_user_pass.sh
+++ change_user_pass.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ldappasswd -x -D "cn=Manager,dc=ccdcldap,dc=local" -W -s NewSecurePassword "uid=lharris,ou=People,dc=ccdcldap,dc=local"
+ldappasswd -x -D "cn=Manager,dc=ccdcldap,dc=local" -W -s <new password unhashed> "uid=lharris,ou=People,dc=ccdcldap,dc=local"
